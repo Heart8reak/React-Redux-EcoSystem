3aabe4b0e47d54645ddc7939c196743d88672255
Carlos Baez <carlosbaez@Green-Planet.attlocal.net>
2020-04-23 00:35:09 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { hot } from 'react-hot-loader'
+import './App.css'
+
+import TodoList from './components/TodoList'
+
+
+const App = () => {
+    return (
+        <div className="App">
+            <TodoList />
+        </div>
+
+    )
+}
+
+export default hot(module)(App)
